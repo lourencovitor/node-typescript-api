76a071a34497fbadafee2bac1a030e8699c39b2c
Vitor Lourenço <vitor.brother17@outlook.com.br>
2020-12-26 13:13:33 -0300

    created mock of axios in project

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,20 @@
+import { StormGlass } from '@src/clients/stormGlass';
+import stormglassNormalizedResponseFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+import * as stormglassWeatherPointFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import axios from 'axios';
+
+jest.mock('axios');
+
+describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
+  it('should return the normalized forecast from the StormGlass service', async () => {
+    const lat = -33.792726;
+    const lng = 151.289824;
+
+    mockedAxios.get.mockResolvedValue({ data: stormglassWeatherPointFixture });
+
+    const stormGlass = new StormGlass(mockedAxios);
+    const response = await stormGlass.fetchPoints(lat, lng);
+    expect(response).toEqual(stormglassNormalizedResponseFixture);
+  });
+});
--- test/globals.d.ts
+++ test/globals.d.ts
@@ -1,5 +1,5 @@
 declare namespace NodeJS {
-    interface Global{
-        testRequest: import('supertest').SuperTest<import('supertest').Test>;
-    }
+  interface Global {
+    testRequest: import('supertest').SuperTest<import('supertest').Test>;
+  }
 }
